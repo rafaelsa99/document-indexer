64c2ebd1cec0f55cbe5c9427a3620920c58707c1
rafaelsa99 <rafa.sa_@hotmail.com>
2020-10-27 22:26:42 +0000

    Measuring time and memory usage

--- src/main/java/DocumentIndexer.java
+++ src/main/java/DocumentIndexer.java
@@ -6,11 +6,21 @@
 
 public class DocumentIndexer {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        /*if (args.length != 1) {
             System.out.println("Error! Parameters: corpus");
             return;
-        }
+        }*/
         Indexer indexer = new Indexer();
-        indexer.makeIndex(args[0]);
+        long freeMemoryBefore = Runtime.getRuntime().freeMemory();
+        long startTime = System.nanoTime();
+        //indexer.makeIndex(args[0]);
+        indexer.makeIndex("ola");
+        long endTime = System.nanoTime();
+        long freeMemoryAfter = Runtime.getRuntime().freeMemory();
+        //Calculate indexing time
+        long duration = (endTime - startTime) / 1000000;  //divide by 1000000 to get milliseconds.
+        System.out.println("Indexing Time: " + duration + " milliseconds");
+        //Calculate Memory Usage
+        System.out.println("Memory used: " + (freeMemoryBefore-freeMemoryAfter));
     }
 }
